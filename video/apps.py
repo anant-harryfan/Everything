d88ff8c7597e0e427b237a2968e2a615dcbe9977
anant <anantchandak12@gmail.com>
2025-04-19 14:05:42 -0400

    kyaPata

--- video/apps.py
+++ video/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppNameConfig(AppConfig):
+class VideoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_name'
+    name = 'video'
---
+++ video/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Video
+
+# Create your views here.
+
+def index(request):
+    videos = Video.objects.all().order_by('-created_at')
+    return render(request, 'video/index.html', {'videos': videos})
